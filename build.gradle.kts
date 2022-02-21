b356fa50c2b2ca258f55b4d60cdd553d5d26470a
h-orito <16453967+h-orito@users.noreply.github.com>
2022-02-21 11:05:20 +0900

    add jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// jackson
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 tasks.withType<KotlinCompile> {
